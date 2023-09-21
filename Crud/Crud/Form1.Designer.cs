e16cbc4360f902795a14dbde03f8672995c23f04
Luan Naves <luandhx@gmail.com>
2023-09-20 22:24:11 -0300

    CRUD IBID

---
+++ Crud/Crud/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Crud {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelId = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonList = new System.Windows.Forms.Button();
+            this.textBoxList = new System.Windows.Forms.TextBox();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.labelNewName = new System.Windows.Forms.Label();
+            this.textBoxNewName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(115, 32);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(50, 147);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Adcionar";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(72, 35);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 3;
+            this.labelName.Text = "Nome";
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(81, 73);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(18, 13);
+            this.labelId.TabIndex = 4;
+            this.labelId.Text = "ID";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(115, 70);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(48, 20);
+            this.textBoxId.TabIndex = 5;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(88, 176);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 6;
+            this.buttonRemove.Text = "Remover";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonList
+            // 
+            this.buttonList.Location = new System.Drawing.Point(298, 147);
+            this.buttonList.Name = "buttonList";
+            this.buttonList.Size = new System.Drawing.Size(75, 23);
+            this.buttonList.TabIndex = 7;
+            this.buttonList.Text = "Listar";
+            this.buttonList.UseVisualStyleBackColor = true;
+            this.buttonList.Click += new System.EventHandler(this.buttonList_Click);
+            // 
+            // textBoxList
+            // 
+            this.textBoxList.Location = new System.Drawing.Point(247, 30);
+            this.textBoxList.Multiline = true;
+            this.textBoxList.Name = "textBoxList";
+            this.textBoxList.Size = new System.Drawing.Size(181, 111);
+            this.textBoxList.TabIndex = 8;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(140, 147);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 9;
+            this.buttonEdit.Text = "Editar";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // labelNewName
+            // 
+            this.labelNewName.AutoSize = true;
+            this.labelNewName.Location = new System.Drawing.Point(47, 108);
+            this.labelNewName.Name = "labelNewName";
+            this.labelNewName.Size = new System.Drawing.Size(64, 13);
+            this.labelNewName.TabIndex = 10;
+            this.labelNewName.Text = "Novo Nome";
+            // 
+            // textBoxNewName
+            // 
+            this.textBoxNewName.Location = new System.Drawing.Point(115, 105);
+            this.textBoxNewName.Name = "textBoxNewName";
+            this.textBoxNewName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNewName.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxNewName);
+            this.Controls.Add(this.labelNewName);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.textBoxList);
+            this.Controls.Add(this.buttonList);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "Gerenciador de Itens";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonList;
+        private System.Windows.Forms.TextBox textBoxList;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Label labelNewName;
+        private System.Windows.Forms.TextBox textBoxNewName;
+    }
+}
+
---
+++ Crud/Crud/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Crud {
+    public partial class Form1 : Form {
+
+        private List<Item> itens = new List<Item>();
+        private int nextID = 1;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e) {
+            string name = textBoxName.Text.Trim();
+
+            if (!string.IsNullOrEmpty(name)) {
+                Item newItem = new Item { ID = nextID, Name = name };
+                itens.Add(newItem);
+                nextID++;
+
+                // Limpar o campo de entrada e exibir uma mensagem
+                textBoxName.Text = "";
+                MessageBox.Show("Item adicionado com sucesso!");
+            }
+            else {
+                MessageBox.Show("Digite um nome válido para adicionar um item.");
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e) {
+            int ID;
+            if (int.TryParse(textBoxId.Text, out ID)) {
+                Item itemToRemove = itens.SingleOrDefault(item => item.ID == ID);
+                if (itemToRemove != null) {
+                    itens.Remove(itemToRemove);
+
+                    // Limpar o campo de entrada e exibir uma mensagem
+                    textBoxId.Text = "";
+                    MessageBox.Show("Item removido com sucesso!");
+                }
+                else {
+                    MessageBox.Show("Item com o ID especificado não encontrado.");
+                }
+            }
+            else {
+                MessageBox.Show("Digite um ID válido para remover um item.");
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e) {
+            int ID;
+            if (int.TryParse(textBoxId.Text, out ID)) {
+                string newName = textBoxNewName.Text.Trim();
+                Item itemToEdit = itens.SingleOrDefault(item => item.ID == ID);
+                if (itemToEdit != null) {
+                    if (!string.IsNullOrEmpty(newName)) {
+                        itemToEdit.Name = newName;
+
+                        // Limpar os campos de entrada e exibir uma mensagem
+                        textBoxId.Text = "";
+                        textBoxNewName.Text = "";
+                        MessageBox.Show("Item editado com sucesso!");
+                    }
+                    else {
+                        MessageBox.Show("Digite um novo nome para editar o item.");
+                    }
+                }
+                else {
+                    MessageBox.Show("Item com o ID especificado não encontrado.");
+                }
+            }
+            else {
+                MessageBox.Show("Digite um ID válido e um novo nome para editar um item.");
+            }
+        }
+
+        private void buttonList_Click(object sender, EventArgs e) {
+            textBoxList.Clear(); // Limpar o TextBox antes de listar os itens
+            foreach (Item item in itens) {
+                textBoxList.AppendText(item.ID + " - " + item.Name + "\r\n");
+            }
+        }
+
+       
+    }
+}
